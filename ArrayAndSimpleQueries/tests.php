848004946ba1a30f21efb12d7ec747034feb0574
Zion Ng <zion@intzone.com>
2019-06-26 09:39:10 +0800

    Add new qn ArrayAndSimpleQueries

---
+++ ArrayAndSimpleQueries/tests.php
@@ -0,0 +1,23 @@
+<?php
+
+require '../src/Tester.php';
+
+$tester = new Tester();
+echo $tester->runTestSuite([
+    [   // Should not have leading spaces for the lines
+        'Test 01',
+        "
+8 4
+1 2 3 4 5 6 7 8
+1 2 4
+2 3 5
+1 4 7
+2 1 4
+        ",
+        "
+1
+2 3 6 5 7 8 4 1
+        ",
+    ],
+
+]);
